3bee942f4992d20c701372eb9e7d4e963ca1def8
joshwolfer <47542028+joshwolfer@users.noreply.github.com>
2019-11-08 16:23:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,185 +1,151 @@
-variable "resource_group" { default = "terraform-wolfer-az-demo" } 
-variable "location" { default = "southcentralus" }
-variable "virtual_network_name" { default = "wolferaznet1" }
-variable "virtual_network_address_space" { default = "10.0.0.0/24" }
-variable "web01-hostname" { default = "wolfer-demo-web01" }
-variable "web02-hostname" { default = "wolfer-demo-web02" }
-variable "haproxy-hostname" { default = "wolfer-demo-haproxy" }
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "jwolferTFE"
+    location = "eastus"
 
-variable "admin_username" {} 
-variable "admin_password" {}
-
-# ========================================== 
-#             Resource Group 
-# ========================================== 
-
-resource "azurerm_resource_group" "josh_demo" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
-	
-  tags = {
-    environment = "Demonstration"
-  }
-
-}
-
-# ========================================== 
-#        Build Virtual Net / subnet 
-# ========================================== 
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.virtual_network_name}"
-  resource_group_name = "${azurerm_resource_group.josh_demo.name}"      # Inherit the Resource Group from "josh_demo" above.
-  location            = "${azurerm_resource_group.josh_demo.location}"  # Inherit the Location from "josh_demo" above.
-  address_space       = ["${var.virtual_network_address_space}"]        # Azure defaults to 10.0.0.0/24
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "josh-demo-subnet"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"           # Tie this subnet definition into the virtual network above.
-  resource_group_name  = "${azurerm_resource_group.josh_demo.name}"       # Inherit the Resource Group from "josh_demo" above.
-  address_prefix       = "${var.virtual_network_address_space}"           # Use the same subnet as the address space.
-}
-
-
-
-# ========================================== 
-#        Security group policy 
-# ========================================== 
-
-resource "azurerm_network_security_group" "josh_demo_secgroup" {
-  name                = "josh_demo-sg"
-  location            = "${azurerm_resource_group.josh_demo.location}"
-  resource_group_name = "${azurerm_resource_group.josh_demo.name}"
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-
-# ========================================== 
-#          Web01 Network Config
-# ========================================== 
-
-resource "azurerm_network_interface" "josh-demo-web01-vnic" {
-  name                      = "josh-demo-web01-vnic"
-  location                  = "${azurerm_resource_group.josh_demo.location}"
-  resource_group_name       = "${azurerm_resource_group.josh_demo.name}"
-  network_security_group_id = "${azurerm_network_security_group.josh_demo_secgroup.id}"   # Uses the security group above.
-
-  ip_configuration {
-    name                          = "josh-demo-web01-ipconfig"
-    subnet_id                     = "${azurerm_subnet.subnet.id}"
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.josh_demo-web01-public_ip.id}"    # Optional public IP. This will need to be the load balancer, when setup.
-  }
-}
-
-resource "azurerm_public_ip" "josh_demo-web01-public_ip" {
-  name                         = "josh_demo-web01-public_ip"
-  location                     = "${azurerm_resource_group.josh_demo.location}"
-  resource_group_name          = "${azurerm_resource_group.josh_demo.name}"
-  allocation_method            = "Dynamic"
-  domain_name_label            = "${var.web01-hostname}"                              
-}
-
-# ------------------------------------------ 
-#             Web01 VM Config
-# ------------------------------------------ 
-
-resource "azurerm_virtual_machine" "web01" {
-  name                = "${var.web01-hostname}"
-  location            = "${azurerm_resource_group.josh_demo.location}"
-  resource_group_name = "${azurerm_resource_group.josh_demo.name}"
-  vm_size             = "Standard_A0"
-
-  network_interface_ids         = ["${azurerm_network_interface.josh-demo-web01-vnic.id}"]
-  delete_os_disk_on_termination = "true"
-
-  #  "az vm image list" To determine the VM details:
-  #
-  #  "offer": "UbuntuServer",
-  #  "publisher": "Canonical",
-  #  "sku": "18.04-LTS",
-  #  "urn": "Canonical:UbuntuServer:18.04-LTS:latest",
-  #  "urnAlias": "UbuntuLTS",
-  #  "version": "latest"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.web01-hostname}-osdisk"
-    managed_disk_type = "Standard_LRS"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-  }
-
-  os_profile {
-    computer_name  = "${var.web01-hostname}"
-    admin_username = "${var.admin_username}"
-    admin_password = "${var.admin_password}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-# ------------------------------------------ 
-#          Provisioning of Web01
-# ------------------------------------------ 
-  provisioner "file" {
-    source      = "config/web01-setup.sh"
-    destination = "/home/${var.admin_username}/setup.sh"
-
-    connection {
-      type     = "ssh"
-      user     = "${var.admin_username}"
-      password = "${var.admin_password}"
-      host     = "${azurerm_public_ip.josh_demo-web01-public_ip.fqdn}"
+    tags = {
+        environment = "jwolfer tfe demo"
     }
-  }
-  
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/${var.admin_username}/setup.sh",
-      "sudo /home/${var.admin_username}/setup.sh",
-    ]
-
-    connection {
-      type     = "ssh"
-      user     = "${var.admin_username}"
-      password = "${var.admin_password}"
-      host     = "${azurerm_public_ip.josh_demo-web01-public_ip.fqdn}"
-    }
-  }
 }
 
-output "Web01_URL" {
-  value = "http://${azurerm_public_ip.josh_demo-web01-public_ip.fqdn}"
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefix       = "10.0.1.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "myNIC"
+    location                  = "eastus"
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.myterraformgroup.name
+    }
+    
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    location                    = "eastus"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "myvm"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCCOkCBHN/OHW0L7SnAtDy8TuNRC4/DMOpRUR/0kiJmruWaRyWLcCV+jsFkdwy3WAEmiIvF988mXycUGiqn+Y992QDgyb8/VpzRw7e4GIjEJqCatJVpNkrWqoVv9kujoGlg+KGX6O9LEfaQZtOCUSu5JpBRYwES7UOSErSKNL2Jqx2RdJIRKhDSrRMSHGNJMzANqMe18PkRWHpMpRABMFxPHAcbvqxwYwQpmtedekOhz8NX4ChRTwne9R167pFNtzVGfx1ABiIPBPLKBucnTJ+n0C4hPJdqogffB3pohNb+lPNR35wZa9s6nqLdTcg+bfju7fUcNETWiSZqK57uK5Uz"
+        }
+    }
+
+    boot_diagnostics {
+        enabled = "true"
+        storage_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "jwolfer tfe demo"
+    }
 }
